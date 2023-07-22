7b3b6f94db2166680fb80ae0f3dca4582bf37aeb
XPPGX <qwerlegendary@gmail.com>
2023-07-22 15:25:14 +0800

    Lib "tTime" upload

---
+++ Lib/tTime/tTime.c
@@ -0,0 +1,8 @@
+#include "tTime.h"
+
+extern inline double seconds(){
+    struct timeval tp;
+    struct timezone tzp;
+    int i = gettimeofday(&tp, &tzp);
+    return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+}
---
+++ Lib/tTime/tTime.h
@@ -0,0 +1,10 @@
+#ifndef COMMON
+#define COMMON
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#endif
+
+#include <sys/time.h>
+
+inline double seconds();
